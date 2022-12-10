84bc7f9de23f4ffab9bd546a02bb343a70833720
Matheus Melo <mth.melo@hotmail.com>
2022-12-10 16:06:28 -0300

    lista04

--- Labb_4/dec2bin.c
+++ Labb_4/dec2bin.c
@@ -6,15 +6,31 @@
 
 #include <stdio.h>
 
+int casaDec(unsigned int n);
+unsigned int decbin(unsigned int n, unsigned int bin, int cd);
+
 int main (void) {
-    unsigned int n;
+    unsigned int n, bin=0;
     printf("Digite um número decimal de no máximo 64 bits:\n");
     scanf("%lld", &n);
-
+    printf("%lld", decbin(n, bin, casaDec(n)));
 
     return 0;
 }
 
-int decbin(unsigned int n){
-    
+unsigned int decbin(unsigned int n, unsigned int bin, int cd){
+    if(n / 2 > 0){
+        bin *= 10 << cd;
+        bin += (decbin(n/2, bin, cd-1));
+    }
+    printf("bin = %lld\tcd = %d\n", bin, cd);
+    return bin << cd + n%2;
+}
+
+int casaDec(unsigned int n){
+    int count = 0;
+    for(int i=1; 1;i *= 10, count++){
+        if(n/i == 0)
+            return count;
+    }
 }

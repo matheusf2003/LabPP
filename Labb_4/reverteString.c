a8c20353ba979580578fd8ab7226815abfe36159
Matheus Melo <mth.melo@hotmail.com>
2022-12-10 11:50:50 -0300

    lista04

--- Labb_4/reverteString.c
+++ Labb_4/reverteString.c
@@ -18,8 +18,8 @@ int main (void) {
     printf("Digite sua mensagem: \n");
     getstr(s, N);
     reverte(s, len(s), rev);
-    upper(rev);
-    printf("%s\n", rev);
+    //upper(rev);
+    //printf("%s\n", rev);
     return 0;
 }
 
@@ -57,7 +57,11 @@ long long int len(char * str) {
 }
 
 void reverte(char * str, long long int nchar, char * rstr){
-    for(int i = nchar-1, i2 = 0; i >= 0;i--, i2++)
-        rstr[i2] = str[i];
-    rstr[nchar] = '\0';
+    char l;
+    for(int i = nchar-1; i >= 0;i--){
+        l = str[i];
+        if(l >= 'a' && l <= 'z')
+            l -= 'a' - 'A';
+        printf("%c", l);
+    }
 }

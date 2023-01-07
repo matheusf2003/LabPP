09a136fda7bac2f8545162dea461ffe1ec364d35
Matheus Melo <mth.melo@hotmail.com>
2023-01-07 19:28:28 -0300

    lista05

--- Labb_5/racionais.c
+++ Labb_5/racionais.c
@@ -15,18 +15,25 @@ Racional;
 
 int mdc(int n1, int n2);
 Racional simplifica(Racional n, int Mdc);
+Racional adicao(Racional n1, Racional n2);
+Racional subtracao(Racional n1, Racional n2);
+Racional multiplicacao(Racional n1, Racional n2);
+Racional divisao(Racional n1, Racional n2);
 
 int main(void) {
-    Racional n1, n2;
+    Racional n1, n2, adi, sub, mult, div;
     printf("Primeiro número: ");
     scanf("%d %d", &n1.numerador, &n1.denominador);
     printf("Segundo número: ");
     scanf("%d %d", &n2.numerador, &n2.denominador);
-    saida(simplifica(n1, mdc(n1.numerador, n1.denominador)), simplifica(n2, mdc(n2.numerador, n2.denominador)));
-    //n1 = simplifica(n1, mdc(n1.numerador, n1.denominador));
-    //n2 = simplifica(n2, mdc(n2.numerador, n2.denominador));
+    n1 = simplifica(n1, mdc(n1.numerador, n1.denominador));
+    n2 = simplifica(n2, mdc(n2.numerador, n2.denominador));
     //printf("\nnumerador = %d\ndenominador = %d", n1.numerador, n1.denominador);
-    printf("%d/%d %d/%d %d/%d %d/%d %d/%d %d/%d", n1.numerador, n1.denominador, n2.numerador, n2.denominador);
+    adi = adicao(n1, n2);
+    sub = subtracao(n1, n2);
+    mult = multiplicacao(n1, n2);
+    div = divisao(n1, n2);
+    printf("%d/%d %d/%d %d/%d %d/%d %d/%d %d/%d", n1.numerador, n1.denominador, n2.numerador, n2.denominador, adi.numerador, adi.denominador, sub.numerador, sub. denominador, mult.numerador, mult.denominador, div.numerador, div.denominador);
     return 0;
 }
 
@@ -48,30 +55,30 @@ int mdc(int n1, int n2) {
     return mdc(n2, n1 % n2);
 }
 
-Racional adição(Racional n1, Racional n2){
+Racional adicao(Racional n1, Racional n2){
     Racional n;
     n.numerador = n1.numerador * n2.denominador + n2.numerador * n1.denominador;
     n.denominador = n1.denominador * n2.denominador;
     return simplifica(n, mdc(n.numerador, n.denominador));
 }
 
-Racional subtração(Racional n1, Racional n2){
+Racional subtracao(Racional n1, Racional n2){
     Racional n;
     n.numerador = n1.numerador * n2.denominador - n2.numerador * n1.denominador;
     n.denominador = n1.denominador * n2.denominador;
     return simplifica(n, mdc(n.numerador, n.denominador));
 }
 
-Racional multiplicação(Racional n1, Racional n2){
+Racional multiplicacao(Racional n1, Racional n2){
     Racional n;
     n.numerador = n1.numerador * n2.numerador;
-    n.denominador = n2.denominador * n2.denominador;
+    n.denominador = n1.denominador * n2.denominador;
     return simplifica(n, mdc(n.numerador, n.denominador));
 }
 
-Racional divisão(Racional n1, Racional n2){
+Racional divisao(Racional n1, Racional n2){
     Racional n;
     n.numerador = n1.numerador * n2.denominador;
-    n.denominador = n.denominador * n2.numerador;
+    n.denominador = n1.denominador * n2.numerador;
     return simplifica(n, mdc(n.numerador, n.denominador));
 }

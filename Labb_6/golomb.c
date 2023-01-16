102886ee28325db2a6a9fc37fa7e02ab8ba239a7
Matheus <mth.melo@hotmail.com>
2023-01-16 16:09:26 -0300

    lista06

--- Labb_6/golomb.c
+++ Labb_6/golomb.c
@@ -8,30 +8,30 @@
 #include <stdlib.h>
 
 typedef
-    long long int
-llint; 
+    long int
+lint;
 
-llint golomb(int n, llint * lista);
-double media(llint * v, int inicio, int final);
+int golomb(lint n, int * lista);
+double media(int * v, lint inicio, lint final);
 
 int main(void){
-    int a, b;
-    llint * lista;
+    lint a, b;
+    int * lista;
     printf("Digite o primeiro indice: ");
-    scanf("%d", &a);
+    scanf("%ld", &a);
     printf("Digite o segundo indice: ");
-    scanf("%d", &b);
-    if(b < a) { int aux = a; a = b; b = aux; }
-    lista = (llint *) malloc((b+1)*sizeof(llint));
-    for(int i = 0; i <= b; i++)
+    scanf("%ld", &b);
+    if(b < a) { lint aux = a; a = b; b = aux; }
+    lista = (int *) malloc((b+1)*sizeof(int));
+    for(lint i = 0; i <= b; i++)
         lista[i] = 0;
     golomb(b, lista);
-    printf("primeiro: %lld\túltimo: %lld\tmédia: %lg\n", lista[a], lista[b], media(lista, a, b));
+    printf("primeiro: %d\túltimo: %d\tmédia: %lg\n", lista[a], lista[b], media(lista, a, b));
     free(lista);
     return 0;
 }
 
-llint golomb(int n, llint * lista){
+int golomb(lint n, int * lista){
     if(lista[n] != 0)
         return  lista[n];
     if(n == 0)
@@ -41,9 +41,9 @@ llint golomb(int n, llint * lista){
     return lista[n];
 }
 
-double media(llint * v, int inicio, int final) {
-    llint m = 0;
-    for(int i = inicio; i <= final; i++)
+double media(int * v, lint inicio, lint final) {
+    lint m = 0;
+    for(lint i = inicio; i <= final; i++)
         m += v[i];
     return ((double) m)/(final - inicio + 1);
 }

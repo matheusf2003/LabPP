dd0fd24103614b6a3518cc4b6cdf812b494034b9
Matheus Melo <mth.melo@hotmail.com>
2023-01-20 09:08:50 -0300

    lista07

--- Labb_7/txtFibonacci.c
+++ Labb_7/txtFibonacci.c
@@ -11,19 +11,34 @@ typedef
     unsigned long long int
 ullint;
 
-ullint Fib(int n, FILE * arq);
+ullint Fib(int n, FILE * arq); // Função usando laço
+ullint Fib_rec(int n, FILE * arq, ullint * fibolist); // Função recursiva
 
 int main (void) {
     int n;
+    ullint * fibolist;
     FILE * arq = fopen("fibonacci.txt", "w");
     if(arq == NULL){
         perror("Erro ao abrir o arquivo.\n");
         return 1;
     }
-    printf("Digite um número N tal que 0 <= N <= 94: ");
+    printf("Digite um número N tal que 1 <= N <= 94: ");
     scanf("%d%*c", &n);
-    printf("Fib[%d] = %llu\n", n, Fib(n, arq));
+    if(n < 1 || n > 94){
+        printf("Digite um número maior que 0 e menor que 95.");
+        return 0;
+    }
+    fibolist = (ullint *) malloc((n + 1) * sizeof(ullint));
+    for(int i = 0; i <= n; i++)
+        fibolist[i] = 0;
+    fprintf(arq, "0\n");
+    if(n > 1){
+        fibolist[1] = 1;
+        fprintf(arq, "1\n");
+    }
+    printf("Fib[%d] = %llu\n", n, Fib_rec(n - 1, arq, fibolist));
     fclose(arq);
+    free(fibolist);
     return 0;
 }
 
@@ -37,4 +52,14 @@ ullint Fib(int n, FILE * arq){
         fprintf(arq, "%llu\n", fibo[i]);
     }
     return fibo[n-1];
+}
+
+ullint Fib_rec(int n, FILE * arq, ullint * fibolist){
+    if(fibolist[n] != 0)
+        return fibolist[n];
+    if(n <= 1)
+        return n;
+    fibolist[n] = Fib_rec(n-1, arq, fibolist) + Fib_rec(n-2, arq, fibolist);
+    fprintf(arq, "%llu\n", fibolist[n]);
+    return fibolist[n];
 }
